e229e4709bc41da629696a5ee63f46cf883889a9
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-15 13:33:03 -0600

    Set up initial files, add scss, remove unused icons

---
+++ src/App/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './App.scss'
+
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {}
+	}
+
+  render() {
+    return (
+		<div className="App">
+      <header className="App-header">
+				Hello
+      </header>
+    </div>
+		)}
+}
+
+export default App
