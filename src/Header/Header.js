20d8bed10e3c98155c5c11f5c236f534392c52ad
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-16 09:16:17 -0600

    Add tagline and favorites icon #3

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import beerMug from '../assets/beer.png'
+import cheers from '../assets/cheers.png'
 import './Header.scss'
 import { NavLink } from 'react-router-dom'
 
 const Header = () => {
 	return (
 		<header className="Header">
-			<div className="title-box">
-				<img src={beerMug} alt="Beer mug" className="logo" />
-				<h1 className="app-title">brewfinder</h1>
+			<div className="slogan">
+				<div className="title-box">
+					<img src={beerMug} alt="Beer mug" className="logo" />
+					<h1 className="app-title">brewfinder</h1>
+				</div>
+				<h2>experience Colorado one hops at a time</h2>
 			</div>
 			<nav>
 				<NavLink to="/"
@@ -19,7 +23,10 @@ const Header = () => {
 					about
 				</NavLink>
 				<NavLink to="/favorites">
-					favorites
+					<div className="favorite-box">
+						<img src={cheers} alt="Two beer mugs clicking together" className="cheers" />
+						favorites
+					</div>
 				</NavLink>
 			</nav>
 		</header>
