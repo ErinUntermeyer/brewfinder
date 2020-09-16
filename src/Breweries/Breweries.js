7d821c6310d5dcf8f4aa22daf862a436833c4d25
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-16 13:47:47 -0600

    Add functionality to change page #5 #7

--- src/Breweries/Breweries.js
+++ src/Breweries/Breweries.js
@@ -18,9 +18,13 @@ const Breweries = () => {
 			})
 	}
 
-	useEffect(() => {
-		getBreweryList(pageNumber)
-	}, [])
+	const changePage = direction => {
+		if (direction === 'back' && pageNumber > 1) {
+			setPageNumber(pageNumber - 1)
+		} else if (direction === 'forward') {
+			setPageNumber(pageNumber + 1)
+		}
+	}
 
 	const makeList = breweries => {
 		return breweries.map(brewery => {
@@ -33,13 +37,25 @@ const Breweries = () => {
 		})
 	}
 
+	useEffect(() => {
+		getBreweryList(pageNumber)
+	}, [pageNumber])
+
 	return (
 		<div>
 			<section className="Breweries">
 				{makeList(breweries)}
 			</section>
-			<button className="back-button">Previous 20</button>
-			<button className="forward-button">Next 20</button>
+			<button
+				className="back-button"
+				onClick={(e) => changePage('back')}>
+				Previous 20
+			</button>
+			<button
+				className="forward-button"
+				onClick={(e) => changePage('forward')}>
+				Next 20
+			</button>
 		</div>
 	)
 }
