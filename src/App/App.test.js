f4a14207408118150e1e4384a7ac9d8b90cb7fe3
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-15 21:56:35 -0600

    Write test that the Header component renders upon load #3

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App Component', () => {
+
+	it('Should render a header upon load', () => {
+		render(<App />)
+		const title = screen.getByRole('heading', { name: /brewfinder/i })
+		expect(title).toBeInTheDocument()
+	})
+
+})
