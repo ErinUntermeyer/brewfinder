1bf37a9ca043af304c01309281c6c1cf35b81710
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-15 22:45:00 -0600

    Add nav links and style #3

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import beerMug from '../assets/beer.png'
 import './Header.scss'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
 	return (
@@ -9,6 +10,18 @@ const Header = () => {
 				<img src={beerMug} alt="Beer mug" className="logo" />
 				<h1 className="app-title">brewfinder</h1>
 			</div>
+			<nav>
+				<NavLink to="/"
+					style={{ textDecoration: "none" }}>
+					home
+				</NavLink>
+				<NavLink to="/about">
+					about
+				</NavLink>
+				<NavLink to="/favorites">
+					favorites
+				</NavLink>
+			</nav>
 		</header>
 	)
 }
