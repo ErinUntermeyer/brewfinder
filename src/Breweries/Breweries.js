ed957c2abb05e4f013909567f199196a548788ec
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-16 13:18:22 -0600

    Turn variable into method that takes in a list to display #7

--- src/Breweries/Breweries.js
+++ src/Breweries/Breweries.js
@@ -4,20 +4,22 @@ import './Breweries.scss'
 import { breweries } from '../helpers/data'
 
 const Breweries = () => {
-	// have 'breweries' be props passed in from App - any array of breweries will display the same. so you can filter
-	const breweriesList = breweries.map(brewery => {
-		return (
-			<article className="brewery-box" key={brewery.id}>
-				<h3>{brewery.name}</h3>
-				<p>{brewery.city}, CO</p>
-			</article>
-		)
-	})
-	
+
+	const makeList = breweries => {
+		return breweries.map(brewery => {
+			return (
+				<article className="brewery-box" key={brewery.id}>
+					<h3>{brewery.name}</h3>
+					<p>{brewery.city}, CO</p>
+				</article>
+			)
+		})
+	}
+
 	return (
 		<div>
 			<section className="Breweries">
-				{breweriesList}
+				{makeList(breweries)}
 			</section>
 			<button className="back-button">Previous 20</button>
 			<button className="forward-button">Next 20</button>
