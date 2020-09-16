9387fdf1d63d7e2472031b57afa79a7579b03d9c
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-16 15:51:59 -0600

    Change to functional component #7

--- src/Breweries/Breweries.js
+++ src/Breweries/Breweries.js
@@ -1,42 +1,33 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './Breweries.scss'
 
-class Breweries extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {}
-	}
-	
-	makeList = breweries => {
-		return breweries.map(brewery => {
-			return (
-				<article className="brewery-box" key={brewery.id}>
-					<h3>{brewery.name}</h3>
-					<p>{brewery.city}, CO</p>
-				</article>
-			)
-		})
-	}
-
-	render() {
+const Breweries = ({ breweries, changePage }) => {
+	const breweryList = breweries.map(brewery => {
 		return (
-			<div>
-				<section className="Breweries">
-					{this.makeList(this.props.breweries)}
-				</section>
-				<button
-					className="back-button"
-					onClick={(e) => this.props.changePage('back')}>
-					Previous 20
-				</button>
-				<button
-					className="forward-button"
-					onClick={(e) => this.props.changePage('forward')}>
-					Next 20
-				</button>
-			</div>
+			<article className="brewery-box" key={brewery.id}>
+				<h3>{brewery.name}</h3>
+				<p>{brewery.city}, CO</p>
+			</article>
 		)
-	}
+	})
+
+	return (
+		<div>
+			<section className="Breweries">
+				{breweryList}
+			</section>
+			<button
+				className="back-button"
+				onClick={(e) => changePage('back')}>
+				Previous 20
+			</button>
+			<button
+				className="forward-button"
+				onClick={(e) => changePage('forward')}>
+				Next 20
+			</button>
+		</div>
+	)
 }
 
 export default Breweries
