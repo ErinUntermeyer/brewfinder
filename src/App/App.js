3361cea0541e3ae2a49278f1a8d3f4d25304a847
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-15 21:18:00 -0600

    Set up App to use hooks

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,15 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.scss'
 
-class App extends Component {
-	constructor() {
-		super()
-		this.state = {}
-	}
+const App = () => {
 
-  render() {
-    return (
+	return (
 		<div className="App">
-      <header className="App-header">
+			<header className="App-header">
 				Hello
-      </header>
-    </div>
-		)}
+			</header>
+		</div>
+	)
 }
 
 export default App
