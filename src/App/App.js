589f729c45605c2f1771ea1f9ce2b371d9c1e532
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-16 15:39:47 -0600

    Remove hooks, use class components

--- src/App/App.js
+++ src/App/App.js
@@ -1,22 +1,78 @@
-import React, { useState, useEffect } from 'react'
+import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
+import { get20BreweriesByPage } from '../helpers/apiCalls'
 import Header from '../Header/Header'
 import Breweries from '../Breweries/Breweries'
 import './App.scss'
 
-const App = () => {
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			breweries: [],
+			pageNumber: 1,
+			warning: '',
+			error: ''
+		}
+	}
 
-	return (
+	componentDidMount() {
+		this.getBreweries()
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		if (this.state.pageNumber !== prevState.pageNumber) {
+			this.getBreweries()
+		}
+	}
+
+	getBreweries = () => {
+		get20BreweriesByPage(this.state.pageNumber)
+			.then(data => {
+				this.setState({ breweries: data })
+			})
+			.catch(error => {
+				this.setState({ error: 'I\'m sorry, we could not retrieve any breweries at this time. Please try again later!' })
+			})
+	}
+
+	changePage = direction => {
+		if (direction === 'back' && this.state.pageNumber === 1) {
+			this.displayWarning('You\'re on the first page!')
+		} else if (direction === 'back' && this.state.pageNumber > 1) {
+			this.setState({ pageNumber: this.state.pageNumber - 1 })
+		} else if (direction === 'forward') {
+			this.setState({ pageNumber: this.state.pageNumber + 1 })
+		}
+	}
+
+	displayWarning = message => {
+		this.setState({ warning: message })
+		setTimeout(() => {
+			this.setState({ warning: '' })
+		}, 3000)
+	}
+
+	render() {
+		return (
 		<div className="App">
 			<Header />
 			<main>
+				{ this.state.error ? <h1 className="error">{this.state.error}</h1> : (
 				<Route exact path="/" render={() => (
-						<Breweries />
+						<Breweries
+							breweries={this.state.breweries}
+							pageNumber={this.state.pageNumber}
+							changePage={this.changePage}
+						/>
 					)}
 				/>
+				) }
+				{ this.state.warning ? <p>{this.state.warning}</p> : null }
 			</main>
 		</div>
-	)
+		)
+	}
 }
 
 export default App
--- src/Breweries/Breweries.js
+++ src/Breweries/Breweries.js
@@ -1,41 +1,13 @@
-import React, {useState, useEffect } from 'react'
-import { get20BreweriesByPage } from '../helpers/apiCalls'
+import React, { Component } from 'react'
 import './Breweries.scss'
 
-const Breweries = () => {
-	const [breweries, setBreweries] = useState([])
-	const [warning, setWarning] = useState('')
-	const [error, setError] = useState('')
-	const [pageNumber, setPageNumber] = useState(1)
-
-	const getBreweryList = pageNumber => {
-		get20BreweriesByPage(pageNumber)
-			.then(data => {
-				setBreweries(data)
-			})
-			.catch(error => {
-				setError('I\'m sorry, we could not retrieve any breweries at this time. Please try again later!')
-			})
+class Breweries extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {}
 	}
-
-	const displayWarning = message => {
-		setWarning(message)
-		setTimeout(() => {
-			setWarning('')
-		}, 3000)
-	}
-
-	const changePage = direction => {
-		if (direction === 'back' && pageNumber === 1) {
-			displayWarning('You\'re on the first page!')
-		} else if (direction === 'back' && pageNumber > 1) {
-			setPageNumber(pageNumber - 1)
-		} else if (direction === 'forward') {
-			setPageNumber(pageNumber + 1)
-		}
-	}
-
-	const makeList = breweries => {
+	
+	makeList = breweries => {
 		return breweries.map(brewery => {
 			return (
 				<article className="brewery-box" key={brewery.id}>
@@ -46,30 +18,25 @@ const Breweries = () => {
 		})
 	}
 
-	useEffect(() => {
-		getBreweryList(pageNumber)
-	}, [pageNumber])
-
-	return (
-		error ? <h1 className="error">{error}</h1> : (
-		<div>
-			<section className="Breweries">
-				{makeList(breweries)}
-			</section>
-			<button
-				className="back-button"
-				onClick={(e) => changePage('back')}>
-				Previous 20
-			</button>
-			<button
-				className="forward-button"
-				onClick={(e) => changePage('forward')}>
-				Next 20
-			</button>
-			<p>{error}</p>
-		</div>
+	render() {
+		return (
+			<div>
+				<section className="Breweries">
+					{this.makeList(this.props.breweries)}
+				</section>
+				<button
+					className="back-button"
+					onClick={(e) => this.props.changePage('back')}>
+					Previous 20
+				</button>
+				<button
+					className="forward-button"
+					onClick={(e) => this.props.changePage('forward')}>
+					Next 20
+				</button>
+			</div>
 		)
-	)
+	}
 }
 
 export default Breweries
