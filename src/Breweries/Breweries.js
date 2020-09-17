3e7ffbe00e82fec25fda58b819cf4d82c729041e
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-16 18:55:32 -0600

    Change to class component, render BreweryDetails #10

--- src/Breweries/Breweries.js
+++ src/Breweries/Breweries.js
@@ -1,33 +1,68 @@
-import React from 'react'
+import React, { Component } from 'react'
+import BreweryDetails from '../BreweryDetails/BreweryDetails'
 import './Breweries.scss'
 
-const Breweries = ({ breweries, changePage }) => {
-	const breweryList = breweries.map(brewery => {
-		return (
-			<article className="brewery-box" key={brewery.id}>
-				<h3>{brewery.name}</h3>
-				<p>{brewery.city}, CO</p>
-			</article>
-		)
-	})
+class Breweries extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			showDetails: false,
+			brewery: null
+		}
+	}
 
-	return (
-		<div>
-			<section className="Breweries">
-				{breweryList}
-			</section>
-			<button
-				className="back-button"
-				onClick={(e) => changePage('back')}>
-				Previous 20
-			</button>
-			<button
-				className="forward-button"
-				onClick={(e) => changePage('forward')}>
-				Next 20
-			</button>
-		</div>
-	)
+	showDetails = () => {
+		this.setState({ showDetails: true })
+	}
+
+	hideDetails = () => {
+		this.setState({ showDetails: false })
+	}
+
+	makeBreweryList = () => {
+		return this.props.breweries.map(brewery => {
+			return (
+				<article className="brewery-box" key={brewery.id}>
+					<h3>{brewery.name}</h3>
+					<p>{brewery.city}, CO</p>
+					<button
+						className="details-button"
+						onClick={() => {
+							this.showDetails()
+							this.setState({ brewery: brewery })
+						}}>
+						details
+					</button>
+				</article>
+			)
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<section className="Breweries">
+					{this.makeBreweryList()}
+				</section>
+				<button
+					className="back-button"
+					onClick={(e) => this.props.changePage('back')}>
+					Previous 20
+				</button>
+				<button
+					className="forward-button"
+					onClick={(e) => this.props.changePage('forward')}>
+					Next 20
+				</button>
+				{ this.state.brewery ? (
+				<BreweryDetails
+					show={this.state.showDetails}
+					handleClose={this.hideDetails}
+					brewery={this.state.brewery}
+				/> ) : null }
+			</div>
+		)
+	}
 }
 
 export default Breweries
