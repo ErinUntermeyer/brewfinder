015eb1bebcabb026e4ef19542d684551e51ca7df
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-16 19:48:01 -0600

    Add and style buttons for details modal #10

--- src/BreweryDetails/BreweryDetails.js
+++ src/BreweryDetails/BreweryDetails.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import cheers from '../assets/cheers.png'
 import './BreweryDetails.scss'
 
 const BreweryDetails = ({ show, handleClose, brewery }) => {
@@ -11,7 +12,16 @@ const BreweryDetails = ({ show, handleClose, brewery }) => {
 				<h2>{brewery.street}</h2>
 				<h3>{brewery.city}, CO {brewery.postal_code.slice(0, 5)}</h3>
 				<p>{brewery.phone.slice(0, 3)}-{brewery.phone.slice(3, 6)}-{brewery.phone.slice(6, 11)}</p>
-				<button onClick={(e) => handleClose()}>close</button>
+				<div className="details-button-box">
+					<a
+						href={brewery.website_url}
+						target="_blank"
+						rel="noopener noreferrer">
+						view website
+        	</a>
+					<button><img src={cheers} alt="Two glasses clinking together" className="cheers-details" />add to favorites</button>
+					<button onClick={(e) => handleClose()}>close</button>
+				</div>
 			</article>
 		</div>
 	)
