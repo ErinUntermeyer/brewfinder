e71d0b7629d17774540e715c027b92e592e5dc3f
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-17 14:11:52 -0600

    Add files and initial structure #16

---
+++ src/Filter/Filter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Filter.scss'
+
+const Filter = () => {
+
+	return (
+		<aside>
+		</aside>
+	)
+	
+}
+
+export default Filter
---
+++ src/Filter/Filter.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Filter from './Filter'
+import { MemoryRouter } from 'react-router-dom'
+import { render, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+describe('Filter Component', () => {
+
+	it('Should', () => {
+
+	})
+
+})
