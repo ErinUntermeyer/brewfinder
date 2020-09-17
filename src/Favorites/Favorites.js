395c13a72cbe1d168a56b4e046dca38a11dc456e
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-17 11:46:35 -0600

    Add files with initial structure #14

---
+++ src/Favorites/Favorites.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import cheers from '../assets/cheers.png'
+import './Favorites.scss'
+
+const Favorites = () => {
+	return (
+		<section className="Favorites">
+			
+		</section>
+	)
+}
+
+export default Favorites
---
+++ src/Favorites/Favorites.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Favorites from './Favorites'
+import { MemoryRouter } from 'react-router-dom'
+import { render, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+describe('Favorites Component', () => {
+
+	it('Should', () => {
+
+	})
+
+})
