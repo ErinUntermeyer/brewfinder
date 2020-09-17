cd4515cb47a670c2ee0bd16e98f1b2395ce38404
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-17 15:12:01 -0600

    Add buttons for each filter type #16

--- src/Filter/Filter.js
+++ src/Filter/Filter.js
@@ -1,13 +1,29 @@
 import React from 'react'
+import barSign from '../assets/bar-sign.png'
 import './Filter.scss'
 
 const Filter = () => {
 
 	return (
-		<aside>
-		</aside>
+		<section className="Filter">
+			<img src={barSign} alt="Bar sign with a beer on it" className="bar-sign"/>
+			<p>filter by<br></br> brewery type</p>
+			<div>
+				<button>micro</button>
+				<button>regional</button>
+				<button>brewpub</button>
+				<button>large</button>
+				<br></br>
+				<button>planning</button>
+				<button>bar</button>
+				<button>contract</button>
+				<button>proprietor</button>
+			</div>
+		</section>
 	)
 	
 }
 
 export default Filter
+
+// types: micro, regional, brewpub, large, planning, bar, contract, proprietor
