78e1d6feb9d84afb6318edd6535b9ee7bc58ea55
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-16 18:56:07 -0600

    Display modal when details button clicked, style #10

--- src/BreweryDetails/BreweryDetails.js
+++ src/BreweryDetails/BreweryDetails.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import './BreweryDetails.scss'
 
-const BreweryDetails = () => {
+const BreweryDetails = ({ show, handleClose, brewery }) => {
+	const toggleClassName = show ? "modal display-block" : "modal display-none"
 
 	return (
-		<article>
-
-		</article>
+		<div className={toggleClassName}>
+			<article className="Brewery-details">
+				<h1>{brewery.name}</h1>
+				<h2>{brewery.street}</h2>
+				<h3>{brewery.city}, CO {brewery.postal_code.slice(0, 5)}</h3>
+				<p>{brewery.phone.slice(0, 3)}-{brewery.phone.slice(3, 6)}-{brewery.phone.slice(6, 11)}</p>
+				<button onClick={(e) => handleClose()}>close</button>
+			</article>
+		</div>
 	)
 }
 
