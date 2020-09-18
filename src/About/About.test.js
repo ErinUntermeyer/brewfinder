78f2d1b386003973f47da87bc139ef6ff874a8de
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-18 13:14:23 -0600

    Write test that four sections display #20

--- src/About/About.test.js
+++ src/About/About.test.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { memo } from 'react'
 import About from './About'
 import { MemoryRouter } from 'react-router-dom'
 import { render, screen, fireEvent } from '@testing-library/react'
@@ -6,8 +6,16 @@ import '@testing-library/jest-dom'
 
 describe('About Component', () => {
 
-	it('Should', () => {
-
+	it('Should display four sections of information', () => {
+		render(<MemoryRouter><About /></MemoryRouter>)
+		const summary = screen.getByRole('heading', { name: /summary/i })
+		const definitions = screen.getByRole('heading', { name: /definitions/i })
+		const relatedLinks = screen.getByRole('heading', { name: /related links/i })
+		const credits = screen.getByRole('heading', { name: /credits/i })
+		expect(summary).toBeInTheDocument()
+		expect(definitions).toBeInTheDocument()
+		expect(relatedLinks).toBeInTheDocument()
+		expect(credits).toBeInTheDocument()
 	})
 	
 })
