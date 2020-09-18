7769e697c1cb6203a45de94b242065971e52eb72
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-18 10:32:50 -0600

    Add articles for each topic #20

--- src/About/About.js
+++ src/About/About.js
@@ -5,7 +5,18 @@ const About = () => {
 
 	return (
 		<section className="About">
-
+			<article className="summary">
+				summary goes here
+			</article>
+			<article className="definitions">
+				definitions go here
+			</article>
+			<article className="related-links">
+				related links go here
+			</article>
+			<article className="credits">
+				credits go here
+			</article>
 		</section>
 	)
 }
