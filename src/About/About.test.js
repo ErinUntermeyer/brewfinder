9421240cb0afe17ae3db6e9b3685174ad6a14b24
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-18 13:20:37 -0600

    Write test that info displays in each section #20

--- src/About/About.test.js
+++ src/About/About.test.js
@@ -17,5 +17,17 @@ describe('About Component', () => {
 		expect(relatedLinks).toBeInTheDocument()
 		expect(credits).toBeInTheDocument()
 	})
+
+	it('Should display information in each section', () => {
+		render(<MemoryRouter><About /></MemoryRouter>)
+		const summarySection = screen.getByText(/designed for the avid beer lovers/i)
+		const definitionsSection = screen.getByText(/combination of a brewery and/i)
+		const relatedLinksSection = screen.getByRole('link', { name: /durango brew train/i })
+		const creditsSection = screen.getByRole('link', { name: /open brewery db/i })
+		expect(summarySection).toBeInTheDocument()
+		expect(definitionsSection).toBeInTheDocument()
+		expect(relatedLinksSection).toBeInTheDocument()
+		expect(creditsSection).toBeInTheDocument()
+	})
 	
 })
