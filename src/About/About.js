25f8dbfd44104b8c5a8235c1527b6c0cd495cc8a
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-18 12:19:05 -0600

    Add text to summary and add initial style #20

--- src/About/About.js
+++ src/About/About.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import orangeRedBeer from '../assets/orangeRed-beer.png'
 import './About.scss'
 
 const About = () => {
@@ -6,15 +7,31 @@ const About = () => {
 	return (
 		<section className="About">
 			<article className="summary">
-				summary goes here
+				<div className="about-title-box">
+					<img src={orangeRedBeer} alt="Beer mug" className="about-logo" />
+					<h1>summary</h1>
+				</div>
+				<p><span className="title-word">brewfinder</span> was designed for the avid beer lovers in the state of Colorado! This application allows any beer lover to search for breweries based on the type of brewery they are interested in, or simply view them all. Click the 'view details' button on each brewery listed to see their address, phone number and a link to their website. Also, please favorite the breweries you know and love so you can easily come back to them! Cheers!</p>
 			</article>
 			<article className="definitions">
+				<div className="about-title-box">
+					<img src={orangeRedBeer} alt="Beer mug" className="about-logo" />
+					<h1>definitions</h1>
+				</div>
 				definitions go here
 			</article>
 			<article className="related-links">
+				<div className="about-title-box">
+					<img src={orangeRedBeer} alt="Beer mug" className="about-logo" />
+					<h1>related links</h1>
+				</div>
 				related links go here
 			</article>
 			<article className="credits">
+				<div className="about-title-box">
+					<img src={orangeRedBeer} alt="Beer mug" className="about-logo" />
+					<h1>credits</h1>
+				</div>
 				credits go here
 			</article>
 		</section>
