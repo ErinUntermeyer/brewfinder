d89aac39056440e6a2cc49bf85ec05377b441ee9
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-18 08:57:04 -0600

    All files and initial structure #20

---
+++ src/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './About.scss'
+
+const About = () => {
+
+	return (
+		<section className="About">
+
+		</section>
+	)
+}
+
+export default About
---
+++ src/About/About.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import About from './About'
+import { MemoryRouter } from 'react-router-dom'
+import { render, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+describe('About Component', () => {
+
+	it('Should', () => {
+
+	})
+	
+})
