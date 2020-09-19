c9bc1fe422c1e5c63afa800f08cd18662efdc46c
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-19 14:27:21 -0600

    Change to class comp, add search with methods to set state #21

--- src/Filter/Filter.js
+++ src/Filter/Filter.js
@@ -1,41 +1,75 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import barSign from '../assets/bar-sign.png'
 import './Filter.scss'
 
-const Filter = ({ setStateByType, type }) => {
+class Filter extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			input: ''
+		}
+	}
 
-	return (
-		<section className="Filter">
-			<img src={barSign} alt="Bar sign with a beer on it" className="bar-sign"/>
-			<p>filter by<br></br> brewery type</p>
+	handleInput(e) {
+		const inputValue = e.target.value
+		this.setState({ input: inputValue })
+	}
+
+	handleSubmit(e) {
+		e.preventDefault()
+		this.props.filterBreweriesByCity(this.state.input)
+	}
+
+	render() {
+		return (
 			<div>
-				<button onClick={(e) => setStateByType("micro")}>
-					micro
-				</button>
-				<button onClick={(e) => setStateByType("brewpub")}>
-					brewpub
-				</button>
-				<button onClick={(e) => setStateByType("regional")}>
-					regional
-				</button>
-				<br></br>
-				<button onClick={(e) => setStateByType("large")}>
-					large
-				</button>
-				<button onClick={(e) => setStateByType("contract")}>
-					contract
-				</button>
-				<button onClick={(e) => setStateByType("proprietor")}>
-					proprietor
-				</button>
-				<button onClick={(e) => setStateByType("all")}>
-					all
-				</button>
-				<p className="current-filter">current filter: {type}</p>
+				<section className="Filter">
+					<img src={barSign} alt="Bar sign with a beer on it" className="bar-sign"/>
+					<p>filter by<br></br> brewery type</p>
+					<div>
+						<button onClick={(e) => this.props.setStateByType("micro")}>
+							micro
+						</button>
+						<button onClick={(e) => this.props.setStateByType("brewpub")}>
+							brewpub
+						</button>
+						<button onClick={(e) => this.props.setStateByType("regional")}>
+							regional
+						</button>
+						<br></br>
+						<button onClick={(e) => this.props.setStateByType("large")}>
+							large
+						</button>
+						<button onClick={(e) => this.props.setStateByType("contract")}>
+							contract
+						</button>
+						<button onClick={(e) => this.props.setStateByType("proprietor")}>
+							proprietor
+						</button>
+						<button onClick={(e) => this.props.setStateByType("all")}>
+							all
+						</button>
+						<p className="current-filter">current filter: {this.props.type}</p>
+					</div>
+				</section>
+				<section className="Filter-search">
+					<form
+						className='search-form'
+						onSubmit={(e) => this.handleSubmit(e)}>
+							<label htmlFor='search'>Search by city:</label>
+							<input
+								className='search-input'
+								type='text'
+								id='search'
+								value={this.state.input}
+								onChange={(e) => this.handleInput(e)}/>
+						<button type='submit'>search</button>
+					</form>
+				</section>
 			</div>
-		</section>
-	)
+		)
+	}
 }
 
 export default Filter
