d56ba73519841906bb2bd995d6ab15a7c8b86bbf
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-21 09:56:55 -0600

    Write test that welcome message appears upon load #37

--- src/Welcome/Welcome.test.js
+++ src/Welcome/Welcome.test.js
@@ -7,7 +7,18 @@ import '@testing-library/jest-dom'
 describe('Welcome Component', () => {
 
 	it('Should display a welcome message', () => {
-
+		render(
+			<MemoryRouter>
+				<Welcome
+					firstLoad={true}
+					closeWelcome={jest.fn()}
+				/>
+			</MemoryRouter>
+		)
+		const title = screen.getByRole('heading', { name: /welcome to/i })
+		const message = screen.getByText(/are you an avid beer lover?/i)
+		expect(title).toBeInTheDocument()
+		expect(message).toBeInTheDocument()
 	})
 	
 })
