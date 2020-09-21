74fcaa2d0ff011e669c311a2f18574fa2db0ba28
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-21 09:07:17 -0600

    Add files and initial structure #37

---
+++ src/Welcome/Welcome.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Welcome from './Welcome'
+import { MemoryRouter } from 'react-router-dom'
+import { render, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+describe('Welcome Component', () => {
+
+	it('Should display a welcome message', () => {
+
+	})
+	
+})
